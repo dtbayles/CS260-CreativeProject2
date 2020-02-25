e5fb9920e179c97ea086f28ecefa54407eb6b484
Drew Bayles <dtbayles@gmail.com>
2020-02-24 23:04:14 -0700

    Added NBA team list

--- js/nba.js
+++ js/nba.js
@@ -7,7 +7,6 @@ document.getElementById("teamSubmit").addEventListener("click", function(event) 
     return;
   else {
     url = "https://api.sportsdata.io/v3/nba/stats/json/Players/" + value + "?key=e09508312add4182b89664da9ebf88b4";
-    //url = "http://api.openweathermap.org/data/2.5/weather?q=Provo,US&units=imperial" + "&APPID=0e289b6ac1639c1a43f3a481ca8b9ad1";
   }
 
   let req = new Request(url, {'Ocp-Apim-Subscription-Key': 'e09508312add4182b89664da9ebf88b4'});
@@ -35,33 +34,28 @@ document.getElementById("teamSubmit").addEventListener("click", function(event) 
                     '</tr>' +
                   '</thead>' +
                   '<tbody id="myTable">';
-console.log(json.length);
       let tableHead = ["FirstName", "LastName", "Jersey", "Position", "Salary", "Weight", "Height"];
-      for (let i = 0; i < tableHead.length; ++i) {
+
+      let FirstNames = [];
+      json.filter(function (e){
+        FirstNames.push(e.FirstName);
+      });
+      console.log(FirstNames);
+
+      for (let i = 0; i < json.length; ++i) {
         results += '<tr>';
-        for (let k = 0; k < json.length; ++k) {
-          let temp = tableHead[i];
-          results += '<td>' + json[k].temp + '</td>';
-        }
+        results += '<td>' + json[i].FirstName + '</td>';
+        results += '<td>' + json[i].LastName + '</td>';
+        results += '<td>' + json[i].Jersey + '</td>';
+        results += '<td>' + json[i].Position + '</td>';
+        results += '<td>$' + json[i].Salary + '</td>';
+        results += '<td>' + json[i].Weight + '</td>';
+        results += '<td>' + json[i].Height + '</td>';
         results += '</tr>';
       }
 
-
 //json[0].FirstName == Anthony
 
-      /*for (let i=0; i < json.weather.length; i++) {
-        results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-      }
-      results += '<h2 class="text-left">Temp: ' + json.main.temp + " &deg;F</h2>"
-      results += '<h2 class="text-left">Feels Like: ' + json.main.feels_like + " &deg;F</h2>"
-      results += '<h2 class="text-left">Humidity: ' + json.main.humidity + '%</h2>';
-      results += "<h2 class='text-left'>Forecast: "
-      for (let i=0; i < json.weather.length; i++) {
-        results += json.weather[i].description
-        if (i !== json.weather.length - 1)
-          results += ", "
-      }
-      results += "</p>" + '</div>' + '</div>'; */
       results += '</tbody>' + '</table>';
       document.getElementById("teamResults").innerHTML = results;
     });
@@ -76,6 +70,8 @@ $(document).ready(function(){
   });
 });
 
+
+/*
 function testJson () {
   const request = new XMLHttpRequest();
   request.open("get", "/LAL.json");
@@ -91,7 +87,64 @@ function testJson () {
 }
 
 function populateRankings(json) {
-  console.log(json);
+  //console.log(json);
+  let results = "";
+
+      results += '<p>All of the current players:</p>' + 
+                  '<input class="form-control" id="myInput" type="text" placeholder="Search..">' +
+              '<br>' +
+                '<table class="table table-bordered table-striped bg-white">' +
+                  '<thead>' +
+                    '<tr>' +
+                      '<th>First</th>' +
+                      '<th>Last</th>' +
+                      '<th>#</th>' +
+                      '<th>Position</th>' +
+                      '<th>Salary</th>' +
+                      '<th>Weight</th>' +
+                      '<th>Height</th>' +
+                    '</tr>' +
+                  '</thead>' +
+                  '<tbody id="myTable">';
+      let tableHead = ["FirstName", "LastName", "Jersey", "Position", "Salary", "Weight", "Height"];
+
+      let FirstNames = [];
+      json.filter(function (e){
+        FirstNames.push(e.FirstName);
+      });
+      let LastNames = [];
+      json.filter(function (e){
+        LastNames.push(e.LastName);
+      });
+      //console.log(FirstNames);
+
+      for (let i = 0; i < json.length; ++i) {
+        results += '<tr>';
+        //for (let k = 0; k < 2; ++k) {
+          //let temp = tableHead[i];
+          //console.log(json[k].temp);
+          //console.log(json[k].FirstName);
+          //results += '<td>' + FirstNames[k] + '</td>';
+          //results += '<td>' + LastNames[k] + '</td>';
+          results += '<td>' + json[i].FirstName + '</td>';
+          results += '<td>' + json[i].LastName + '</td>';
+          results += '<td>' + json[i].Jersey + '</td>';
+          results += '<td>' + json[i].Position + '</td>';
+          results += '<td>$' + json[i].Salary + '</td>';
+          results += '<td>' + json[i].Weight + '</td>';
+          results += '<td>' + json[i].Height + '</td>';
+        //}
+        results += '</tr>';
+      }
+
+
+//json[0].FirstName == Anthony
+
+      //results += "</p>" + '</div>' + '</div>';
+      results += '</tbody>' + '</table>';
+      document.getElementById("teamResults").innerHTML = results;
 }
 
 document.addEventListener("DOMContentLoaded", () => {testJson();});
+
+*/
